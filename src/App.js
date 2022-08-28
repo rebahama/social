d7b3b036ebab86b37068855976ac8062efa61fa1
rebahama <wiri@hotmail.se>
2022-08-28 03:20:31 +0000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-     <h1>testing</h1>
+     <Button variant="success">Success</Button>
     </div>
   );
 }
