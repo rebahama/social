1216ce23318ac5320a49819a76c91801fd60b79b
rebahama <wiri@hotmail.se>
2022-08-31 01:34:38 +0000

    add navbar clicks

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,7 @@ axios.defaults.baseURL ='https://django-rest-hama.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
